38b9ee16d6d764c6adef6f06cd7ddc54dabb2e5f
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-01-24 10:45:08 +0530

    Added new Methods

---
+++ src/commonFunctions/Sample.java
@@ -0,0 +1,27 @@
+package commonFunctions;
+
+public class Sample {
+public static void add()
+{
+	int a=12,b=89,c;
+	c=a+b;
+	System.out.println(c);
+}
+public static void div()
+{
+	int a=12,b=89,c;
+	c=a+b;
+	System.out.println(c);
+}
+public static void mul()
+{
+	int a=12,b=89,c;
+	c=a+b;
+	System.out.println(c);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
